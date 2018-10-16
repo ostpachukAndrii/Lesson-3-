87d334c167ffcfb6c3e22a6ccd494abede09f58c
ostapchukandrii <ostapchukandrii@gmail.com>
2018-10-16 14:53:44 +0300

    Lesson 3

--- Lesson/src/app/app.module.ts
+++ Lesson/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CartModule } from './cart/components/cart.module';
-import { OrdersModule } from './orders/components/orders.module';
-import { ProductsModule } from './products/components/products.module';
+import { CartModule } from './cart/cart.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
 
 
 @NgModule({
--- Lesson/src/app/products/products.module.ts
+++ Lesson/src/app/products/products.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BookComponent } from './book/book.component';
-import { BookListComponent } from './book-list/book-list.component';
+import { BookComponent } from './components/book/book.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
 @NgModule({
   imports: [
